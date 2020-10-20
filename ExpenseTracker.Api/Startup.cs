726dabb68803d08edd29cb14c3acdb336e4de395
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-10-20 20:37:00 +0000

    Add swagger support

--- ExpenseTracker.Api/Startup.cs
+++ ExpenseTracker.Api/Startup.cs
@@ -35,6 +35,7 @@ namespace ExpenseTracker
             {
                 services.AddDbContext<ExpenseTrackerContext>(options => options.UseSqlite($"Data Source=ExpenseTracker.Api/expensetracker.db"));
             }
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,16 @@ namespace ExpenseTracker
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
