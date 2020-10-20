8c455f2dbeef9a5e85798951945d507a0fac17ef
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-10-20 20:29:47 +0000

    Format Startup.cs

--- ExpenseTracker.Api/Startup.cs
+++ ExpenseTracker.Api/Startup.cs
@@ -30,7 +30,6 @@ namespace ExpenseTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             if (env.IsDevelopment())
             {
