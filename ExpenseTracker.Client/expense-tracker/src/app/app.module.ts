7dfbf987e3a3f54a00aa33ea4d020b3185b17cf1
Ferenc Hammerl <hammerl.ferenc@gmail.com>
2020-10-21 22:13:43 +0100

    Add ng-ant as a UI library

--- ExpenseTracker.Client/expense-tracker/src/app/app.module.ts
+++ ExpenseTracker.Client/expense-tracker/src/app/app.module.ts
@@ -4,6 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ExpenseModule } from './expense/expense.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -12,9 +22,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
-    ExpenseModule
+    ExpenseModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzBreadCrumbModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
